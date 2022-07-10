2579def7c808725ee536c321aa6b57f0b9774c6c
VadimN <ranger47@mail.ru>
2022-07-10 18:05:47 +0600

    Example11_ArrayLibrary

---
+++ Example11_ArrayLibrary/Program.cs
@@ -0,0 +1,41 @@
+﻿void FillAray(int[] collection){
+    int length = collection.Length;
+    int index = 0;
+    while(index < length){
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] coll){
+    int count = coll.Length;
+    int position = 0;
+    while(position < count){
+        Console.WriteLine(coll[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[]collection, int finde){
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while( index < count){
+      if(collection[index] == finde){
+         position = index;
+         break;
+      }
+      index++;
+    }
+    return position;
+}
+ int[] array = new int[10];
+
+FillAray(array);
+PrintArray(array);
+array[4] = 4;
+array[6] = 4;
+Console.WriteLine();
+
+int pos = IndexOf(array, 44);
+Console.WriteLine(pos);

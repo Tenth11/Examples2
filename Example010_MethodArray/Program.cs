270d556af9c4288b9b0f4a493c3aa9d069fb8047
VadimN <ranger47@mail.ru>
2022-07-10 17:31:40 +0600

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 42, 55, 64, 55, 6, 57, 68};
+
+int n = array.Length;
+
+int finde = 55;
+
+int index = 0;
+
+while(index < n){
+    if(array[index] == finde)
+    {Console.WriteLine(index);
+    break;
+    }
+    index++;
+}
+
